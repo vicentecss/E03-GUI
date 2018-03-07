01c07e18c7df88c288dd176f8ef4e11a2ad81cf6
João Eduardo Montandon <edu.montandon@gmail.com>
2018-03-07 11:01:00 -0300

    Added starter project

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_adapters/MainActivity.java
@@ -0,0 +1,13 @@
+package br.ufmg.coltec.tp.e03_adapters;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
